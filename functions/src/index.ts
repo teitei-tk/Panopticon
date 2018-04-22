92805f7aaa3bbd41cc2a668455239f8e54d39359
teitei-tk <teitei.tk@gmail.com>
2018-04-22 23:41:19 +0900

    use express app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+const app = express();
+
+app.get('/timestamp', (request, response) => {
+  response.send(`${Date.now()}`);
 });
+
+export const funcApp = functions.https.onRequest(app);
